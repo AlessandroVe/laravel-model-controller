5656a67b8d3d4b190b97731d67a93c82ea15de95
AlessandroVe <aveneziano536@gmail.com>
2021-09-09 16:40:38 +0200

    .

--- app/Http/Controllers/ControllerHome.php
+++ app/Http/Controllers/ControllerHome.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\movie;
+
 class ControllerHome extends Controller
 {
     public function index(){
-        $dato = 'io sono dato';
-        $dato1 = 'ciao';
-        $dato2 = 'ok';
-        
-        return view('home',dump(compact('dato', 'dato1','dato2')));
+         
+        $all_Movies = movie::all();
+        dump($all_Movies);
+        return view('home');
     }
 }
